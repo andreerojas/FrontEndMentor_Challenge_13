a569ba96c95eb9ba6b15c8cfa466bcb61e7e4b5a
Andree Rojas <andre_r_g@hotmail.com>
2022-10-14 17:00:25 +0200

    themes completely change according to the value of the toggle button and the calculator's screen shows the result of calculations

--- app.js
+++ app.js
@@ -1,20 +1,97 @@
 const themeOptions = document.querySelectorAll('input[type="radio"]');
-function applyTheme(){
-    const main = document.querySelector('main');
+const nums = document.querySelectorAll('.btn-num');
+const screen = document.querySelector('.result-container');
+const operators = document.querySelectorAll('.btn-operator');
+const reset = document.querySelector('#reset');
+const equal = document.querySelector('#equal');
+
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+function applyTheme() {
+    let elements = [document.querySelector('main'),
+    document.querySelector('.options-container'),
+    document.querySelector('.keyboard-container'),
+    document.querySelector('.result-container'),
+    ...document.querySelectorAll('span'),
+    ...document.querySelectorAll('.calc-button'),
+    document.querySelector('input[type="radio"]:checked + label .options')];
     const value = document.querySelector('input[type="radio"]:checked').value;
-    for(let idx = 1 ; idx <=3; idx++){
-        if(parseInt(value)=== idx){
-            main.classList.add(`theme-${idx}`);
+
+    for (let idx = 1; idx <= 3; idx++) {
+        if (parseInt(value) === idx) {
+            for (let element of elements) {
+                element.classList.add(`theme-${idx}`);
+            }
         }
-        else{
-            main.classList.remove(`theme-${idx}`);
+        else {
+            for (let element of elements) {
+                element.classList.remove(`theme-${idx}`);
+            }
         }
-    }    
+    }
+
 }
 
-
-for(let option of themeOptions){
-    option.addEventListener('input',applyTheme);
+function calculate() {
+    let result = 0;
+    if (firstNum && secondNum && operator) {
+        switch (operator) {
+            case "+":
+                result = parseFloat(firstNum) + parseFloat(secondNum);
+                break;
+            case "-":
+                result = parseFloat(firstNum) - parseFloat(secondNum);
+                break;
+            case "×":
+                result = parseFloat(firstNum) * parseFloat(secondNum);
+                break;
+            case "/":
+                result = parseFloat(firstNum) / parseFloat(secondNum);
+                break;
+        }
+        firstNum = result.toString();
+        secondNum = "";
+        screen.innerText = result;
+    }
 }
 
+for (let option of themeOptions) {
+    option.addEventListener('input', applyTheme);
+}
+
+for (let num of nums) {
+    num.addEventListener('click', function () {
+        if (!operator) {
+            if ((num.innerText !== ".") || (num.innerText === "." && !firstNum.includes("."))) {
+                firstNum += num.innerText;
+                screen.innerText = firstNum;
+            }
+
+        }
+        else {
+            if ((num.innerText !== ".") || (num.innerText === "." && !secondNum.includes("."))) {
+                secondNum += num.innerText;
+                screen.innerText = `${firstNum} ${operator} ${secondNum}`;
+            }
+        }
+    })
+}
+
+for (let op of operators) {
+    op.addEventListener('click', function () {
+        calculate();
+        screen.innerText += (" " + op.innerText);
+        operator = op.innerText;
+    })
+}
+
+reset.addEventListener('click', function () {
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+    screen.innerText = "0";
+})
+
+equal.addEventListener('click', calculate);
 applyTheme();
